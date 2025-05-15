34ce9897759168ca6b36188cc0f10c82612ac6a9
Rapid-Rawr <yagabut4@gmail.com>
2025-05-15 12:22:47 +0700

    grading - TableDriven dan Generic

---
+++ GenericMethod/Grading/Grading/Grader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Grader<T>
+{
+    private readonly Dictionary<double, string> gradeTable = new()
+    {
+        { 85, "A" },
+        { 75, "B" },
+        { 65, "C" },
+        { 60, "D" },
+        { 0, "E" }
+    };
+
+    public string Calculate(T correctAnswers, T totalQuestions)
+    {
+      
+
+        double correct = Convert.ToDouble(correctAnswers);
+        double total = Convert.ToDouble(totalQuestions);
+
+        if (total == 0)
+            throw new InvalidOperationException("Total soal tidak boleh 0.");
+
+        double percentage = (correct / total) * 100;
+
+        if (percentage < 0 || percentage > 100)
+            throw new InvalidOperationException("Nilai persentase tidak valid.");
+
+        foreach (var grade in gradeTable)
+        {
+            if (percentage >= grade.Key)
+                return $"Nilai: {percentage}%, Grade: {grade.Value}";
+        }
+
+        return "Nilai tidak valid.";
+    }
+}
---
+++ GenericMethod/Grading/Grading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+
+            int correct = int.Parse("7");
+
+            int total = int.Parse("10");
+
+            var grader = new Grader<int>();
+            Console.WriteLine( 
+            grader.Calculate(correct, total)
+                
+                );
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
