dc7dbe22830c1063db3db4dd6b7dd49cc459ce12
acid <mkhafidh46@gmail.com>
2025-06-15 14:58:21 +0700

    add gui load Soal

---
+++ WinFormsApp1/WinFormsApp1/Views/Controls/SoalNext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.Views.Controls
+{
+    public partial class SoalNext : UserControl
+    {
+        public SoalNext()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void skor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
